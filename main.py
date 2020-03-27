151b9152396c7e15452c7f4f2c6cd50e3a6ff00b
Harsh Rana <hr23232323@gmail.com>
2020-03-27 17:12:01 -0400

    graph added, needs some work

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 import pandas as pd
 import numpy as np
 import glob
+import matplotlib.pyplot as plt
+
 
 def main():
 	#df = pd.read_csv("raw_data/1990_1.csv")
 	#print(df)
 	list_of_files = glob.glob("raw_data/*")
-
+	num = 0
 	for file in list_of_files:
+		num+=1
 		df_temp = pd.read_csv(file)
 		df = process_df(df_temp)
-		print(df)
+		#print(df)
+
+		palette = plt.get_cmap('Set1')
+		plt.plot(df['day'], df['change'], marker='', color=palette(num), linewidth=1, alpha=0.9)
+		plt.title("Drawdown Graph", loc='left', fontsize=12, fontweight=0, color='orange')
+		plt.xlabel("Days")
+		plt.ylabel("Change")
+	plt.show()
+
+
+
 
 
 def process_df(dataframe):
