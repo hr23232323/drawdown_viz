dd1db9b47429d7304a81829eb3e34804a26b4477
Harsh Rana <hr23232323@gmail.com>
2020-03-28 12:14:47 -0400

    adding side chart with breakdown

--- main.py
+++ main.py
@@ -7,41 +7,9 @@ import matplotlib.ticker as mtick
 
 
 def main():
-	#df = pd.read_csv("raw_data/1990_1.csv")
-	#print(df)
 	list_of_files = glob.glob("raw_data/*")
-	# style
-	plt.style.use('seaborn-darkgrid')
-	# create a color palette
-	palette = plt.get_cmap('tab10')
-	fig, ax = plt.subplots(figsize=(10, 6))
-
-	num=0
-	for file in list_of_files:
-		num+=1
-		file_label = re.search("\\d+", file).group(0)
-		print(file, file_label)
-		df_temp = pd.read_csv(file)
-		df = process_df(df_temp)
-		
-		if(file == "raw_data\\2020_1.csv"):
-			ax.plot(df['day'], df['value'], marker='', color="red", linewidth=1.5, alpha=1, label=file_label)
-		else:
-			ax.plot(df['day'], df['value'], marker='', color=palette(num), linewidth=1, alpha=0.6, label=file_label)
-
-	# Add legend
-	plt.legend(loc=1, ncol=2)
-	plt.title("Comparing the COVID-19 bear market to others from history", loc='left', fontsize=12, fontweight=0, color='black')
-	plt.xlabel("Days into the bear market", fontsize=12)
-	plt.ylabel("Value of a $10,000 investment", fontsize=12)
-	fmt = '${x:,.0f}'	
-	tick = mtick.StrMethodFormatter(fmt)
-	ax.yaxis.set_major_formatter(tick) 
-	plt.xticks(rotation=25)	
-	#plt.xscale("log")
-	plt.show()
-
-
+	#create_main_chart(list_of_files)
+	create_side_chart(list_of_files)
 
 
 
@@ -59,11 +27,51 @@ def process_df(dataframe):
 	return dataframe
 
 
+def create_main_chart(list_of_files):
+	plt.style.use('seaborn-darkgrid')
+	# create a color palette
+	palette = plt.get_cmap('tab10')
+	fig, ax = plt.subplots(figsize=(10, 6))
+
+	num=0
+	for file in list_of_files:
+		num+=1
+		file_label = re.search("\\d+", file).group(0)
+		
+		#print(file, file_label)
+		df_temp = pd.read_csv(file)
+		df = process_df(df_temp)
+		
+		if(file == "raw_data\\2020_1.csv"):
+			ax.plot(df['day'], df['value'], marker='', color="red", linewidth=1.5, alpha=1, label=file_label)
+		else:
+			ax.plot(df['day'], df['value'], marker='', color=palette(num), linewidth=1, alpha=0.6, label=file_label)
+
+	# Add legend
+	plt.legend(loc=1, ncol=2)
+	plt.title("Comparing the COVID-19 bear market to others from history", loc='left', fontsize=12, fontweight=0, color='black')
+	plt.xlabel("Days into the bear market", fontsize=12)
+	plt.ylabel("Value of a $10,000 investment", fontsize=12)
+	fmt = '${x:,.0f}'	
+	tick = mtick.StrMethodFormatter(fmt)
+	ax.yaxis.set_major_formatter(tick) 
+	plt.xticks(rotation=25)	
+	plt.show()
+
+
+
+def create_main_chart(list_of_files):
+	plt.style.use('seaborn-darkgrid')
+	# create a color palette
+	palette = plt.get_cmap('tab10')
+	fig, ax = plt.subplots(figsize=(10, 6))
+
+	for file in list_of_files:
+		file_label = re.search("\\d+", file).group(0)
+		df_temp = pd.read_csv(file)
+		df = process_df(df_temp)
 
 
 if __name__ == "__main__":
     # execute only if run as a script
     main()
-
-
-
